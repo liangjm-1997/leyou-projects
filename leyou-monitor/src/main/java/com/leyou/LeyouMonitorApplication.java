a063237afe74919b125686cafc9e0f79d3d4e7ff
liangjm <liangjm@si-tech.com.cn>
2021-05-11 11:27:05 +0800

    删除无效代码

--- leyou-monitor/src/main/java/com/leyou/LeyouMonitorApplication.java
+++ leyou-monitor/src/main/java/com/leyou/LeyouMonitorApplication.java
@@ -1,15 +1,15 @@
 package com.leyou;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableZuulProxy
-public class LeyouZuulApplication {
+@EnableAdminServer
+public class LeyouMonitorApplication {
     public static void main(String[] args) {
-        SpringApplication.run(LeyouZuulApplication.class, args);
+        SpringApplication.run(LeyouMonitorApplication.class,args);
     }
 }
